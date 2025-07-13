3b25fae4fc8e7b214a98240f267dcb3366f632d9
SEOSIEVE <royalcircle97@naver.com>
2025-07-13 21:05:28 +0900

    FEAT: Dist 폴더 추가, production, development 빌드 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,57 @@
-const path = require('path');
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
 
-module.exports = {
-  mode: 'production',
-  entry: './src/code.ts',
+module.exports = (env, argv) => ({
+  mode: argv.mode === 'production' ? 'production' : 'development',
+
+  // This is necessary because Figma's 'eval' works differently than normal eval
+  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+
+  entry: {
+    code: './src/code.ts', // The entry point for your plugin code
+  },
+
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+      // Converts TypeScript code to JavaScript
+      { 
+        test: /\.tsx?$/, 
+        use: 'ts-loader', 
+        exclude: /node_modules/
       },
-    ],
+
+      // Enables including CSS by doing "import './file.css'" in your TypeScript code
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"],
+      },
+      // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
+      { 
+        test: /\.svg/,
+        type: 'asset/inline'
+      }
+    ]
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+
+  // Webpack tries these extensions for you if you omit the extension like "import './file'"
+  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+
   output: {
-    filename: 'code.js',
-    path: path.resolve(__dirname, '.'),
-    library: {
-      name: 'VestellaMap',
-      type: 'var',
-    },
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
   },
-  optimization: {
-    minimize: false,
-  },
-  externals: {
-    '@figma/plugin-typings': 'figma',
-  },
-}; 
+
+  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
+  plugins: [
+    new webpack.DefinePlugin({
+      'global': {} // Fix missing symbol error when running in developer VM
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/ui.html',
+      filename: 'ui.html'
+    })
+  ],
+}) 
